a0acc223ff7d7cdc32cb51ba6630e1aa9959e18b
luis <luisfj_pr@hotmail.com>
2024-07-30 20:02:09 +0000

    Update for CI CD pass

--- src/test/java/dev/luisjohann/ofxmsimport/OfxMsImportApplicationTests.java
+++ src/test/java/dev/luisjohann/ofxmsimport/OfxMsImportApplicationTests.java
@@ -1,11 +1,15 @@
 package dev.luisjohann.ofxmsimport;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+@SpringJUnitConfig
 @SpringBootTest
+@ActiveProfiles("dev")
 class OfxMsImportApplicationTests {
 
 	@Test
