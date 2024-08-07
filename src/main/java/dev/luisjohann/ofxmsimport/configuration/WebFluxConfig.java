638dcc944d329f9a0d3e31cee8eb30d0e0506431
luis <luisfj_pr@hotmail.com>
2024-08-07 10:51:53 +0000

    Add container prod memory limit, adjust project dependencies and configurations, refactor file upload method to webflux pattern

---
+++ src/main/java/dev/luisjohann/ofxmsimport/configuration/WebFluxConfig.java
@@ -0,0 +1,10 @@
+package dev.luisjohann.ofxmsimport.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
+public class WebFluxConfig {
+
+}
